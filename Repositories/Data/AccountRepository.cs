96e551581e8dcf7d9427b0af5eaaf91af6ce49ab
shaniamakatuuk <shaniamakatuuk@gmail.com>
2022-10-07 11:08:09 +0800

    Project

---
+++ Repositories/Data/AccountRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SIBKMNET_WebApp.Context;
+using SIBKMNET_WebApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIBKMNET_WebApp.Repositories.Data
+{
+    public class AccountRepository
+    {
+        MyContext myContext;
+        public AccountRepository(MyContext myContext)
+        {
+            this.myContext = myContext;
+        }
+        public ResponseLogin Login(Login login)
+        {
+            var data = myContext.UserRoles
+                 .Include(x => x.Role)
+                 .Include(x => x.User)
+                 .Include(x => x.User.Employee)
+                 .FirstOrDefault(x => x.User.Employee.Email.Equals(login.Email) &&
+                                      x.User.Password.Equals(login.Password));
+            if (data != null)
+            {
+                var response = new ResponseLogin()
+                {
+                    Id = data.User.Employee.Id,
+                    FullName = data.User.Employee.FullName,
+                    Email = data.User.Employee.Email,
+                    Role = data.Role.Name
+                };
+                return response;
+            }
+            return null;
+        }
+    }
+}
